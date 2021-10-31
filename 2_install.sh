379cfa6511b8a49535ce4909e6dbc043f92dbb6f
Timur <48119843+timamus@users.noreply.github.com>
2021-10-31 20:51:49 +0600

    Update 2_install.sh

--- 2_install.sh
+++ 2_install.sh
@@ -12,6 +12,23 @@ sudo pacman -S bitwarden
 echo "Installing firefox"
 sudo pacman -S firefox
 
+firefox &
+sleep 10 & PID=$!
+printf "Waiting for firefox to create a user profile ["
+while kill -0 $PID 2> /dev/null; do
+ printf  "."
+ sleep 1
+done
+printf "] ";echo
+
+sudo pkill -f firefox
+
+# Configration firefox
+section=Profile0
+var=Path
+profile_name=$(awk -F"=" -v section=$section -v var=$var '$1=="["section"]"{secFound=1}secFound==1 && $1==var{print $2; secFound=0}' /home/$USER/.mozilla/firefox/profiles.ini) # Get name user's profile name
+echo $profile_name
+
 # Installing libreoffice with russian language pack
 echo "Installing libreoffice with russian language pack"
 sudo pacman -S libreoffice-still libreoffice-still-ru
