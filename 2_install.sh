29b24418a3620df728c988e5d47e7487c2c28408
Timur <48119843+timamus@users.noreply.github.com>
2021-11-03 16:31:57 +0600

    Update 2_install.sh

--- 2_install.sh
+++ 2_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Installing nemo-fileroller
+echo "Installing nemo-fileroller"
+sudo pacman -S nemo-fileroller
+
 # Installing telegram
 echo "Installing telegram"
 sudo pacman -S telegram-desktop
@@ -12,23 +16,6 @@ sudo pacman -S bitwarden
 echo "Installing firefox"
 sudo pacman -S firefox
 
-firefox &
-sleep 10 & PID=$!
-printf "Waiting for firefox to create a user profile ["
-while kill -0 $PID 2> /dev/null; do
- printf  "."
- sleep 1
-done
-printf "] ";echo
-
-sudo pkill -f firefox
-
-# Configration firefox
-section=Profile0
-var=Path
-profile_name=$(awk -F"=" -v section=$section -v var=$var '$1=="["section"]"{secFound=1}secFound==1 && $1==var{print $2; secFound=0}' /home/$USER/.mozilla/firefox/profiles.ini) # Get name user's profile name
-echo $profile_name
-
 # Installing libreoffice with russian language pack
 echo "Installing libreoffice with russian language pack"
 sudo pacman -S libreoffice-still libreoffice-still-ru
@@ -36,6 +23,10 @@ sudo pacman -S libreoffice-still libreoffice-still-ru
 # Installing steam from flatpak repo
 echo "Installing steam from flatpak repo"
 sudo flatpak install flathub com.valvesoftware.Steam
+
+# Installing luckybackup from AUR
+echo "Installing luckybackup from AUR"
+pamac build luckybackup
 
 echo "Installation successfull"
 read -rsn1 -p"Press any key to exit";echo
