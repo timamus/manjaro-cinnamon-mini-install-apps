eaa4cfd7cf03f70fecea7cba7988b194c9b899e8
Timur <48119843+timamus@users.noreply.github.com>
2021-11-30 16:45:33 +0600

    Update 1_system_upgrade.sh

--- 1_system_upgrade.sh
+++ 1_system_upgrade.sh
@@ -3,19 +3,19 @@
 set -Eeuo pipefail
 
 # Update mirror list and set fastest download server
-echo -en "\033[1;33m Update mirror list and set fastest download server... \n"
+echo -en "\033[1;33m Update mirror list and set fastest download server... \033[0m \n"
 sudo pacman-mirrors --fasttrack --api --protocol https
 
 # Start by updating and upgrading all packages installed in the system
-echo -en "\033[1;33m Start by updating and upgrading all packages installed in the system... \n"
+echo -en "\033[1;33m Start by updating and upgrading all packages installed in the system... \033[0m \n"
 sudo pacman -Syyu
 
 # Installing Flatpak
-echo -en "\033[1;33m Installing Flatpak... \n"
+echo -en "\033[1;33m Installing Flatpak... \033[0m \n"
 pamac install flatpak libpamac-flatpak-plugin
 
 # Enable Flatpak for pamac (uncomment the line #EnableFlatpak)
-echo -en "\033[1;33m Enable Flatpak for pamac... \n"
+echo -en "\033[1;33m Enable Flatpak for pamac... \033[0m \n"
 if grep -q "EnableFlatpak" /etc/pamac.conf; then
    sudo sed -Ei '/EnableFlatpak/s/^#//' /etc/pamac.conf
    # To comment it back
@@ -25,7 +25,7 @@ else
 fi
 
 # Enable Flatpak updates for pamac (uncomment the line #CheckFlatpakUpdates)
-echo -en "\033[1;33m Enable Flatpak updates for pamac... \n"
+echo -en "\033[1;33m Enable Flatpak updates for pamac... \033[0m \n"
 if grep -q "CheckFlatpakUpdates" /etc/pamac.conf; then
    sudo sed -Ei '/CheckFlatpakUpdates/s/^/#/' /etc/pamac.conf
 else
@@ -33,14 +33,14 @@ else
 fi
 
 # Enable AUR for pamac (uncomment the line #EnableAUR)
-echo -en "\033[1;33m Enable AUR for pamac... \n"
+echo -en "\033[1;33m Enable AUR for pamac... \033[0m \n"
 sudo sed -Ei '/EnableAUR/s/^#//' /etc/pamac.conf
 # To comment it back
 # sudo sed -Ei '/EnableAUR/s/^/#/' /etc/pamac.conf
 
 # Enable AUR updates for pamac (uncomment the line #CheckAURUpdates)
-echo -en "\033[1;33m Enable AUR updates for pamac... \n"
+echo -en "\033[1;33m Enable AUR updates for pamac... \033[0m \n"
 sudo sed -Ei '/CheckAURUpdates/s/^/#/' /etc/pamac.conf
 
-echo -en "\033[0;35m Installation successfull \n"
+echo -en "\033[0;35m Installation successfull \033[0m \n"
 echo 'A system reboot is recommended. Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot;
