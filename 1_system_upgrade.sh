6e907fde8afbe88e8f98525a3f8594cfec813171
Timur <48119843+timamus@users.noreply.github.com>
2021-11-30 14:32:17 +0600

    Update 1_system_upgrade.sh

--- 1_system_upgrade.sh
+++ 1_system_upgrade.sh
@@ -10,32 +10,33 @@ sudo pacman -Syyu
 pamac install flatpak libpamac-flatpak-plugin
 
 # Enable Flatpak for pamac (uncomment the line #EnableFlatpak)
+PATH=/etc/pamac.conf # Path to pamac.conf file
 echo "Enable Flatpak for pamac"
-if grep -q "EnableFlatpak" /etc/pamac.conf; then
-   sudo sed -Ei '/EnableFlatpak/s/^#//' /etc/pamac.conf
+if grep -q "EnableFlatpak" $PATH; then
+   sudo sed -Ei '/EnableFlatpak/s/^#//' $PATH
    # To comment it back
    # sudo sed -Ei '/EnableFlatpak/s/^/#/' /etc/pamac.conf
 else
-   sudo sed -i -e '$a\\' -e "\$ a EnableFlatpak" /etc/pamac.conf
+   sudo sed -i -e '$a\\' -e "\$ a EnableFlatpak" $PATH
 fi
 
 # Enable Flatpak updates for pamac (uncomment the line #CheckFlatpakUpdates)
 echo "Enable Flatpak updates for pamac"
-if grep -q "CheckFlatpakUpdates" /etc/pamac.conf; then
-   sudo sed -Ei '/CheckFlatpakUpdates/s/^/#/' /etc/pamac.conf
+if grep -q "CheckFlatpakUpdates" $PATH; then
+   sudo sed -Ei '/CheckFlatpakUpdates/s/^/#/' $PATH
 else
-   sudo sed -i -e '$a\\' -e "\$ a CheckFlatpakUpdates" /etc/pamac.conf
+   sudo sed -i -e '$a\\' -e "\$ a CheckFlatpakUpdates" $PATH
 fi
 
 # Enable AUR for pamac (uncomment the line #EnableAUR)
 echo "Enable AUR for pamac"
-sudo sed -Ei '/EnableAUR/s/^#//' /etc/pamac.conf
+sudo sed -Ei '/EnableAUR/s/^#//' $PATH
 # To comment it back
 # sudo sed -Ei '/EnableAUR/s/^/#/' /etc/pamac.conf
 
 # Enable AUR updates for pamac (uncomment the line #CheckAURUpdates)
 echo "Enable AUR updates for pamac"
-sudo sed -Ei '/CheckAURUpdates/s/^/#/' /etc/pamac.conf
+sudo sed -Ei '/CheckAURUpdates/s/^/#/' $PATH
 
 echo "Installation successfull"
 echo 'A system reboot is recommended. Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot;
