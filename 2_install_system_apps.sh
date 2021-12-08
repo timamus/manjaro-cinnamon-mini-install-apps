fc8464ea312e83b6b8fd82d02b0ae2d7a109f7b7
Timur <48119843+timamus@users.noreply.github.com>
2021-12-08 16:49:35 +0600

    Update 2_install_system_apps.sh

--- 2_install_system_apps.sh
+++ 2_install_system_apps.sh
@@ -3,20 +3,20 @@
 set -Eeuo pipefail
 
 # Installing timeshift
-echo "Installing timeshift"
+-en "\033[1;33m Installing timeshift... \033[0m \n"
 sudo pacman -S timeshift
 
 # Installing gufw
-echo "Installing gufw"
+echo "Installing gufw... \033[0m \n"
 sudo pacman -S gufw
 
 # Installing dconf-editor
-echo "Installing dconf-editor"
+echo "Installing dconf-editor... \033[0m \n"
 sudo pacman -S dconf-editor
 
 # Installing gnome-system-monitor
 # https://unix.stackexchange.com/questions/174683/custom-global-keybindings-in-cinnamon-via-gsettings
-echo "Installing gnome-system-monitor"
+echo "Installing gnome-system-monitor... \033[0m \n"
 sudo pacman -S gnome-system-monitor
 gsettings set org.cinnamon.desktop.keybindings custom-list \ "['custom0']"
 gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ name "System monitor"
@@ -24,44 +24,44 @@ gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/d
 gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ binding "['<Primary><Shift><Ctrl>Escape']"
 
 # Installing baobab
-echo "Installing baobab"
+echo "Installing baobab... \033[0m \n"
 sudo pacman -S baobab
 
 # Installing gnome-disk-utility
-echo "Installing gnome-disk-utility"
+echo "Installing gnome-disk-utility... \033[0m \n"
 sudo pacman -S gnome-disk-utility
 
 # Installing gnome-font-viewer
-echo "Installing gnome-font-viewer"
+echo "Installing gnome-font-viewer... \033[0m \n"
 sudo pacman -S gnome-font-viewer
 
 # Installing mintstick
-echo "Installing mintstick"
+echo "Installing mintstick... \033[0m \n"
 sudo pacman -S mintstick
 
 # Installing gnome-calculator
-echo "Installing gnome-calculator"
+echo "Installing gnome-calculator... \033[0m \n"
 sudo pacman -S gnome-calculator
 
 # Installing nemo-fileroller
-echo "Installing nemo-fileroller"
+echo "Installing nemo-fileroller... \033[0m \n"
 sudo pacman -S nemo-fileroller
 
 # Installing xviewer with plugins
-echo "Installing xviewer with plugins"
+echo "Installing xviewer with plugins... \033[0m \n"
 sudo pacman -S xviewer xviewer-plugins
 
 # Installing xreader with support for djvu files
-echo "Installing xreader with support for djvu files"
+echo "Installing xreader with support for djvu files... \033[0m \n"
 sudo pacman -S xreader djvulibre
 
 # Installing gnome-screenshot
-echo "Installing gnome-screenshot"
+echo "Installing gnome-screenshot... \033[0m \n"
 sudo pacman -S gnome-screenshot
 
 # Installing gprename
-echo "Installing gprename"
+echo "Installing gprename... \033[0m \n"
 sudo pacman -S gprename
 
-echo "Installation successfull"
+echo -en "\033[0;35m Installation successfull \033[0m \n"
 echo 'A system reboot is recommended. Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot;
