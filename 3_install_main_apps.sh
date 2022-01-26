3ea43c860f5c06bdefd455885097633452130fc8
Timur <48119843+timamus@users.noreply.github.com>
2022-01-26 18:25:18 +0600

    Update 3_install_main_apps.sh

--- 3_install_main_apps.sh
+++ 3_install_main_apps.sh
@@ -1,66 +1,68 @@
 #!/usr/bin/env bash
 
+set -Eeuo pipefail
+
 # Installing transmission
-echo "Installing transmission"
-sudo pacman -S transmission-gtk
+echo -en "\033[1;33m Installing transmission... \033[0m \n"
+sudo pacman -S transmission-gtk --noconfirm
 
 # Installing veracrypt
-echo "Installing veracrypt"
-sudo pacman -S veracrypt
+echo -en "\033[1;33m Installing veracrypt... \033[0m \n"
+sudo pacman -S veracrypt --noconfirm
 
 # Installing remmina with rdp and vnc plugins
-echo "Installing remmina with rdp and vnc plugins"
-sudo pacman -S remmina freerdp libvncserver
+echo -en "\033[1;33m Installing remmina with rdp and vnc plugins... \033[0m \n"
+sudo pacman -S remmina freerdp libvncserver --noconfirm
 
 # Installing gimp with help
-echo "Installing gimp with help"
-sudo pacman -S gimp gimp-help-ru
+echo -en "\033[1;33m Installing gimp with help... \033[0m \n"
+sudo pacman -S gimp gimp-help-ru --noconfirm
 
 # Installing shotcut
-echo "Installing shotcut"
-sudo pacman -S shotcut
+echo -en "\033[1;33m Installing shotcut... \033[0m \n"
+sudo pacman -S shotcut --noconfirm
 
 # Installing smplayer with skins
-echo "Installing smplayer with skins and themes"
-sudo pacman -S smplayer smplayer-skins smplayer-themes
+echo -en "\033[1;33m Installing smplayer with skins and themes... \033[0m \n"
+sudo pacman -S smplayer smplayer-skins smplayer-themes --noconfirm
 sed -i 's/^\(gui\s*=\s*\).*$/\1MiniGUI/' /$HOME/.config/smplayer/smplayer.ini
 sed -i 's/^\(iconset\s*=\s*\).*$/\1PapirusDark/' /$HOME/.config/smplayer/smplayer.ini
 sed -i 's/^\(qt_style\s*=\s*\).*$/\1kvantum-dark/' /$HOME/.config/smplayer/smplayer.ini
 
 # Installing firefox
-echo "Installing firefox"
-sudo pacman -S firefox
+echo -en "\033[1;33m Installing firefox... \033[0m \n"
+sudo pacman -S firefox --noconfirm
 
 # Deleting midori browser and all the dependencies
-echo "Deleting midori browser and all the dependencies"
+echo -en "\033[1;33m Deleting midori browser and all the dependencies... \033[0m \n"
 sudo pacman -Rcns midori
 rm -R /home/$USER/.config/midori
 rm -R /home/$USER/.cache/midori
 rm -R /home/$USER/.local/share/zeitgeist
 
 # Installing telegram
-echo "Installing telegram"
-sudo pacman -S telegram-desktop
+echo -en "\033[1;33m Installing telegram... \033[0m \n"
+sudo pacman -S telegram-desktop --noconfirm
 
 # Installing bitwarden
-echo "Installing bitwarden"
-sudo pacman -S bitwarden
+echo -en "\033[1;33m Installing bitwarden... \033[0m \n"
+sudo pacman -S bitwarden --noconfirm
 
 # Installing libreoffice with russian language pack
-echo "Installing libreoffice with russian language pack"
-sudo pacman -S libreoffice-still libreoffice-still-ru
+echo -en "\033[1;33m Installing libreoffice with russian language pack... \033[0m \n"
+sudo pacman -S libreoffice-still libreoffice-still-ru --noconfirm
 
 # Installing steam from flatpak repo
-echo "Installing steam from flatpak repo"
+echo -en "\033[1;33m Installing steam from flatpak repo... \033[0m \n"
 sudo flatpak install flathub com.valvesoftware.Steam
 
 # Installing luckybackup from AUR
-echo "Installing luckybackup from AUR"
+echo -en "\033[1;33m Installing luckybackup from AUR... \033[0m \n"
 pamac build luckybackup
 
 # Installing ProtonVPN from AUR
-echo "Installing ProtonVPN from AUR"
+echo -en "\033[1;33m Installing ProtonVPN from AUR... \033[0m \n"
 pamac build protonvpn
 
-echo "Installation successfull"
-read -rsn1 -p"Press any key to exit";echo
+echo -en "\033[0;35m Installation successfull \033[0m \n"
+echo 'A system reboot is recommended. Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot;
