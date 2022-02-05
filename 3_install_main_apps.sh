1bb4e1b0fea5ca37444893e4d678abdf4098c9bd
Timur <48119843+timamus@users.noreply.github.com>
2022-02-05 18:52:55 +0600

    Update 3_install_main_apps.sh

--- 3_install_main_apps.sh
+++ 3_install_main_apps.sh
@@ -95,7 +95,7 @@ echo -en "\033[1;33m Installing steam from flatpak repo... \033[0m \n"
 sudo flatpak install flathub com.valvesoftware.Steam
 
 if [ $(locale | sed -n 's/^LANG=//p') == "ru_RU.UTF-8" ]; then
-  sudo pacman -S --noconfirm firefox-i18n-ru gimp-help-ru libreoffice-still-ru
+  sudo pacman -S --noconfirm firefox-i18n-ru gimp-help-ru libreoffice-still-ru hunspell-ru
   # Installing hyphen-ru from AUR
   echo -en "\033[1;33m Installing hyphen-ru from AUR... \033[0m \n"
   yay -S --noconfirm hyphen-ru
