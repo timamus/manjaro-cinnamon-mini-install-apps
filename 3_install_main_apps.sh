88144d7702c2835bea2c766aebb7b1c727068f64
Timur <48119843+timamus@users.noreply.github.com>
2022-02-08 15:56:03 +0600

    Update 3_install_main_apps.sh

--- 3_install_main_apps.sh
+++ 3_install_main_apps.sh
@@ -13,6 +13,7 @@ sudo pacman -S --noconfirm veracrypt
 # Installing virtualbox
 echo -en "\033[1;33m Installing virtualbox... \033[0m \n"
 sudo pacman -S --noconfirm virtualbox
+sudo gpasswd -a $USER vboxusers
 
 # Installing remmina with rdp and vnc plugins
 echo -en "\033[1;33m Installing remmina with rdp and vnc plugins... \033[0m \n"
