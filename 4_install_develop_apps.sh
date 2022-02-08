9a7ecd0ba5cf1e6094b7bc64911e128a89c70b02
Timur <48119843+timamus@users.noreply.github.com>
2022-02-08 16:31:32 +0600

    Update 4_install_develop_apps.sh

--- 4_install_develop_apps.sh
+++ 4_install_develop_apps.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Installing rider from flatpak repo
+echo -en "\033[1;33m Installing rider from flatpak repo... \033[0m \n"
+sudo flatpak install flathub com.jetbrains.Rider
+
 NodeJS
 VSCode
 DBeawer
