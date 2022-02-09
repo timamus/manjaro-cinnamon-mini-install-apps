81862480440a8b284c5c0e085c0704359076a579
Timur <48119843+timamus@users.noreply.github.com>
2022-02-09 15:56:08 +0600

    Update 4_install_develop_apps.sh

--- 4_install_develop_apps.sh
+++ 4_install_develop_apps.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 # Installing rider from flatpak repo
-echo -en "\033[1;33m Installing rider from flatpak repo... \033[0m \n"
-sudo flatpak install flathub com.jetbrains.Rider
+#echo -en "\033[1;33m Installing rider from flatpak repo... \033[0m \n"
+#sudo flatpak install flathub com.jetbrains.Rider
+
+
+
+# Installing rider
+yay -S rider
 
 NodeJS
 VSCode
