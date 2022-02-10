71521e61ae26de594fad3e430a0a627e5ec2236f
Timur <48119843+timamus@users.noreply.github.com>
2022-02-10 12:39:21 +0600

    Update 4_install_develop_apps.sh

--- 4_install_develop_apps.sh
+++ 4_install_develop_apps.sh
@@ -4,10 +4,14 @@
 #echo -en "\033[1;33m Installing rider from flatpak repo... \033[0m \n"
 #sudo flatpak install flathub com.jetbrains.Rider
 
+# Installing mono
+sudo pacman -S --noconfirm mono
 
+# Installing dotnet
+yay -S rider dotnet-sdk-bin
 
 # Installing rider
-yay -S rider dotnet-sdk-bin xamarin-android
+yay -S rider xamarin-android
 
 NodeJS
 VSCode
