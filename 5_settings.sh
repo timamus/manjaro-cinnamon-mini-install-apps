4afbebfed3aba2de02a9fcafdaee4e692927689e
Timur <48119843+timamus@users.noreply.github.com>
2022-02-11 14:35:13 +0600

    Update 5_settings.sh

--- 5_settings.sh
+++ 5_settings.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
+
+
+
+echo -en "\033[0;35m System settings are completed \033[0m \n"
+echo 'A system reboot is recommended. Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot;
