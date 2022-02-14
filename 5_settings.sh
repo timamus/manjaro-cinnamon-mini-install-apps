73955e4366b60db0bb881e7364c49d8cda22ce72
Timur <48119843+timamus@users.noreply.github.com>
2022-02-14 14:33:16 +0600

    Update 5_settings.sh

--- 5_settings.sh
+++ 5_settings.sh
@@ -9,23 +9,28 @@ sudo pacman -S --noconfirm timeshift-autosnap-manjaro
 sudo sed -i 's/skipRsyncAutosnap=true/skipRsyncAutosnap=false/' /etc/timeshift-autosnap.conf
 
 # Creating a swap file with automatic determination of its size for hibernation
-echo -en "\033[1;33m Creating a swap file with automatic determination of its size for hibernation... \033[0m \n"
-TOTAL_MEMORY_G=$(awk '/MemTotal/ { print ($2 / 1048576) }' /proc/meminfo)
-TOTAL_MEMORY_ROUND=$(echo "$TOTAL_MEMORY_G" | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}')
-TOTAL_MEMORY_SQRT=$(echo "$TOTAL_MEMORY_G" | awk '{print sqrt($1)}')
-ADD_SWAP_SIZE=$(echo "$TOTAL_MEMORY_SQRT" | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}')
-SWAP_SIZE_WITH_HYBER=$(($TOTAL_MEMORY_ROUND + $ADD_SWAP_SIZE))
-sudo dd if=/dev/zero of=/swapfile bs=1G count=$SWAP_SIZE_WITH_HYBER status=progress
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo bash -c "echo /swapfile none swap defaults 0 0 >> /etc/fstab"
-SWAP_DEVICE=$(findmnt -no UUID -T /swapfile)
-SWAP_FILE_OFFSET=$(sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}')
-sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="resume=UUID='"$SWAP_DEVICE"' resume_offset='"$SWAP_FILE_OFFSET"' /' /etc/default/grub
-sudo sed -i '52 s/fsck/resume fsck/' /etc/mkinitcpio.conf
-sudo mkinitcpio -P
-sudo update-grub
+is_swap=$(swapon -s)
+if [[ -z "$is_swap" ]]; then
+  echo -en "\033[1;33m Creating a swap file with automatic determination of its size for hibernation... \033[0m \n"
+  TOTAL_MEMORY_G=$(awk '/MemTotal/ { print ($2 / 1048576) }' /proc/meminfo)
+  TOTAL_MEMORY_ROUND=$(echo "$TOTAL_MEMORY_G" | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}')
+  TOTAL_MEMORY_SQRT=$(echo "$TOTAL_MEMORY_G" | awk '{print sqrt($1)}')
+  ADD_SWAP_SIZE=$(echo "$TOTAL_MEMORY_SQRT" | awk '{print ($0-int($0)<0.499)?int($0):int($0)+1}')
+  SWAP_SIZE_WITH_HYBER=$(($TOTAL_MEMORY_ROUND + $ADD_SWAP_SIZE))
+  sudo dd if=/dev/zero of=/swapfile bs=1G count=$SWAP_SIZE_WITH_HYBER status=progress
+  sudo chmod 600 /swapfile
+  sudo mkswap /swapfile
+  sudo swapon /swapfile
+  sudo bash -c "echo /swapfile none swap defaults 0 0 >> /etc/fstab"
+  SWAP_DEVICE=$(findmnt -no UUID -T /swapfile)
+  SWAP_FILE_OFFSET=$(sudo filefrag -v /swapfile | awk '$1=="0:" {print substr($4, 1, length($4)-2)}')
+  sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="resume=UUID='"$SWAP_DEVICE"' resume_offset='"$SWAP_FILE_OFFSET"' /' /etc/default/grub
+  sudo sed -i '52 s/fsck/resume fsck/' /etc/mkinitcpio.conf
+  sudo mkinitcpio -P
+  sudo update-grub
+else
+  echo "Do whatever you want as \$variable is not empty"
+fi
 
 # Installing and configuring plymouth
 echo -en "\033[1;33m Installing and configuring plymouth... \033[0m \n"
